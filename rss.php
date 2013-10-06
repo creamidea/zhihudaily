8e18ea78a4e4362fb704376ee4f958be9b766db3
Mario叔叔 <faceair.zh@gmail.com>
2013-10-06 14:45:35 +0800

    rss输出

---
+++ rss.php
@@ -0,0 +1,40 @@
+<?php
+$mysql = new SaeMysql();
+header('Content-type: text/xml; charset=utf-8');
+
+$str .= '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+$str .= '<rss version="2.0"' . "\n";
+$str .= 'xmlns:content="http://purl.org/rss/1.0/modules/content/"' . "\n";
+$str .= 'xmlns:wfw="http://wellformedweb.org/CommentAPI/"' . "\n";
+$str .= 'xmlns:dc="http://purl.org/dc/elements/1.1/"' . "\n";
+$str .= 'xmlns:atom="http://www.w3.org/2005/Atom"' . "\n";
+$str .= 'xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"' . "\n";
+$str .= 'xmlns:slash="http://purl.org/rss/1.0/modules/slash/"' . "\n";
+$str .= '>' . "\n";
+$str .= '<channel>' . "\n";
+$str .= '<title>知乎日报</title>' . "\n";
+$url = 'http://'  . $_SERVER['SERVER_NAME'] . ($_SERVER["SERVER_PORT"] == 80 ? '' : ':' . $_SERVER["SERVER_PORT"]) . $_SERVER["REQUEST_URI"]; 
+$str .= '<atom:link href="' . $url . '" rel="self" type="application/rss+xml" />' . "\n";
+$str .= '<link>http://daily.zhihu.com/</link>' . "\n";
+$str .= '<description>知乎日报</description>' . "\n";
+$str .= '<lastBuildDate>'. date(DATE_RSS) . '</lastBuildDate>' . "\n";
+$str .= '<language>zh-CN</language>' . "\n";
+$str .= '<sy:updatePeriod>faceair</sy:updatePeriod>' . "\n";
+$str .= '<generator>http://zhihudaily.sinaapp.com/</generator>' . "\n";
+
+$items = $mysql->getData('SELECT body,share_url,title FROM `daily` ORDER BY  `daily`.`ga_prefix` DESC LIMIT 0 , 20');
+foreach($items as $item){ 
+    $str .= '<item>' . "\n";
+    $str .= '<title><![CDATA[' . $item['title'] . ']]></title>' . "\n";
+    $str .= '<link>' . $item['share_url'] . '</link>' . "\n";
+    $str .= '<pubDate>' . date(DATE_RSS, strtotime('2013'.$item['time'])) . '</pubDate>' . "\n";
+    $str .= '<dc:creator>知乎日报</dc:creator>' . "\n";
+    $str .= '<guid isPermaLink="false">' . $item['share_url'] . '</guid>' . "\n";
+    $str .= '<description><![CDATA[' . $item['body'] . ']]></description>' . "\n";
+    $str .= '<content:encoded><![CDATA[' . $item['body'] . ']]></content:encoded>' . "\n";
+    $str .= '</item>' . "\n";
+} 
+
+$str .= '</channel>' . "\n";
+$str .= '</rss>' . "\n";
+echo $str;
