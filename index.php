0071837a251dc754793a97c892e7951f18b4a648
Mario叔叔 <faceair.zh@gmail.com>
2013-10-07 16:16:23 +0800

    转为mysql存储

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 	<div class="main-wrap">
 		<div class="search">
 			<a target="_self" href="http://zhihudaily.sinaapp.com/search.php" class="button"><span>搜索</span></a>
-            		<a target="_self" href="http://zhihudaily.sinaapp.com/rss.xml" class="rss"><span>RSS</span></a>
+			<a target="_self" href="http://zhihudaily.sinaapp.com/rss.xml" class="rss"><span>RSS</span></a>
 		</div>
 		<a href="/" target="_self" title="知乎日报"><i class="web-logo"></i></a>
 	</div>
@@ -24,51 +24,36 @@
 	<div class="headline">
 
 		<div class="img-wrap">
-    
 <?php 
+$mysql = new SaeMysql();
 
 if(!$_GET["before"]){
-    
-    if($_GET["refresh"] == "1"){
-        unlink('saestor://zhihudaily/' .date('Ymd'). '.txt');
-    }
-    
-    if(is_file('saestor://zhihudaily/' .date('Ymd'). '.txt')){
-		$webcode = json_decode(file_get_contents('saestor://zhihudaily/' .date('Ymd'). '.txt'), 1);
-    }else{
-		$webcode = json_decode(file_get_contents('http://news.at.zhihu.com/api/1.2/news/latest'), 1);
-    }
+    $day = date('Ymd');
 }else{
-    $beforeday = date('Ymd',strtotime($_GET["before"]) - 3600*24);
-    
-    if($_GET["refresh"] == "1"){
-        unlink('saestor://zhihudaily/' .$beforeday. '.txt');
-    }
-    
-    if(is_file('saestor://zhihudaily/' .$beforeday. '.txt')){
-        $webcode = json_decode(file_get_contents('saestor://zhihudaily/' .$beforeday. '.txt'), 1);
-    }else{
-    	$resource = file_get_contents('http://news.at.zhihu.com/api/1.2/news/before/' . $_GET["before"]);
-        if(!strstr($resource ,'<html><title>404: Not Found</title><body>404: Not Found</body></html>') and $resource != '{}'){
-    		$webcode = json_decode($resource, 1);
-    		file_put_contents('saestor://zhihudaily/' .$webcode['date']. '.txt',$resource);
-        }else{
-			echo '<script type="text/javascript">window.location.href="http://zhihudaily.sinaapp.com/"</script>'; 
-        }
-    }
+	$day = date('Ymd',strtotime($_GET["before"]) - 3600*24);
 }
 
-echo '		<h1 class="headline-title">' .$webcode['display_date']. '</h1>
-			<span class="img-source">图片：' .$webcode['news']['0']['image_source']. '</span>
-			<img src="' .$webcode['news']['0']['image']. '" alt="">
-		</div>';
+$webcode = $mysql->getData("SELECT * FROM `daily` WHERE date = '$day' ORDER BY `daily`.`ga_prefix` DESC");
+if(count($webcode) == 0){
+	echo '<script type="text/javascript">window.location.href="http://zhihudaily.sinaapp.com/"</script>'; 
+}
 
-for($i=0;$i<count($webcode['news']);$i++){
-echo '	<div class="headline-background">
-			<a href="' .$webcode['news'][$i]['share_url']. '" target="_blank"  class="headline-background-link">
-			<div class="heading-content">' .$webcode['news'][$i]['title']. '</div>
+$weekarray = array("日","一","二","三","四","五","六");
+$display_date = date('Y.m.d',strtotime($day)) . " 星期".$weekarray[date("w",strtotime($day))];
+$image_source = $webcode['0']['image_source'];
+$image = $webcode['0']['image'];
+
+echo '			<h1 class="headline-title">' .$display_date. '</h1>
+			<span class="img-source">图片：' .$image_source. '</span>
+			<img src="' .$image. '" alt="' .$image_source.'">
+		</div>'."\n";
+
+for($i=0;$i<count($webcode);$i++){
+echo '		<div class="headline-background">
+			<a href="' .$webcode[$i]['share_url']. '" target="_blank"  class="headline-background-link">
+			<div class="heading-content">' .$webcode[$i]['title']. '</div>
 			</a>
-		</div>';
+		</div>'."\n";
 }
 
 ?>
@@ -79,18 +64,10 @@ echo '	<div class="headline-background">
 
 <div class="footer">
     <div class="f">
-		<?php echo '<a target="_self" href="http://zhihudaily.sinaapp.com/index.php?before=' .$webcode['date']. '" class="page-btn">前一天</a>';?>
+		<?php echo '<a target="_self" href="http://zhihudaily.sinaapp.com/index.php?before=' .$day. '" class="page-btn">前一天</a>';?>
     </div>
-	<br>&copy; 2013 知乎 &middot; Powered by <a href="https://github.com/faceair/zhihudaily">faceair</a> &middot;
+	<br>&copy; 2013 知乎 &middot; Powered by <a href="https://github.com/faceair/zhihudaily">faceair</a>
 
-<?php
-if(!$_GET["before"]){
-    echo '<a target="_self" href="http://zhihudaily.sinaapp.com/index.php?refresh=1">强制刷新</a>';
-}else{
-    echo '<a target="_self" href="http://zhihudaily.sinaapp.com/index.php?before=' .$_GET["before"]. '&refresh=1">强制刷新</a>';
-}
-?>
-    
 <script>
 var _hmt = _hmt || [];
 (function() {
